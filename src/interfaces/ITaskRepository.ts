2bf382711ed359280864a392dddab284ac313788
Nanndo9 <gofernando44@gmail.com>
2025-05-09 20:26:45 -0300

    feat: define interface ITaskRepository para inversão de dependência

---
+++ src/interfaces/ITaskRepository.ts
@@ -0,0 +1,10 @@
+import { Task } from "../entities/Task";
+
+export interface ITaskRepository {
+    findById(id: string): Promise<Task | null>;
+    findByUserId(userId: string): Promise<Task[]>;
+    save(task: Task): Promise<Task>;
+    update(id: string, taskData: Partial<Task>): Promise<void>;
+    disableTask(id: string): Promise<void>;
+    restore(id: string): Promise<void>;
+}
